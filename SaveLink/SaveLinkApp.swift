83cc5f948ac12461af98c2ca72aabc7eee1d09a3
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-05 14:41:36 -0300

    Initial Commit

---
+++ SaveLink/SaveLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveLinkApp.swift
+//  SaveLink
+//
+//  Created by Christian Manzaraz on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SaveLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
