f698b4848fdfab38e8add69999e3a249173cb6c4
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-09 13:49:17 -0300

    Creamos ProfileView a la UI, TabView en HomeView, LinkedAccounts Model, movemos el User a la carpeta Model, Creamos método para extraer los proveedrores del currentUser, Creamos getCurrentProvider en AuthenticationProvider y AuthenticationViewModel, Creamos métodos que son helpers

---
+++ SaveLink/Authentication/Repository/Model/LinkedAccounts.swift
@@ -0,0 +1,8 @@
+//
+//  LinkedAccounts.swift
+//  SaveLink
+//
+//  Created by Christian Manzaraz on 09/10/2023.
+//
+
+import Foundation
---
+++ SaveLink/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SaveLink
+//
+//  Created by Christian Manzaraz on 09/10/2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
