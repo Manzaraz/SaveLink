bbd76dade53b000b0e44208458f7dace251e6ab8
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-09 23:54:25 -0300

    Creé método createNewLink en LinkViewModel

--- SaveLink/Links/Repository/LinkRepository.swift
+++ SaveLink/Links/Repository/LinkRepository.swift
@@ -9,13 +9,22 @@ import Foundation
 
 final class LinkRepository {
     private let linkDataSource: LinkDataSource
+    private let metadataDatasource: MetadataDatasource
     
-    init(linkDataSource: LinkDataSource = LinkDataSource()) {
+    init(
+        linkDataSource: LinkDataSource = LinkDataSource(),
+        metadataDatasource: MetadataDatasource = MetadataDatasource()
+    ) {
         self.linkDataSource = linkDataSource
+        self.metadataDatasource = metadataDatasource
     }
     
     func getAllLinks(completionBlock: @escaping (Result<[LinkModel], Error>) -> Void) {
         linkDataSource.getAllLinks(completionBlock: completionBlock)
+    }
+    
+    func createNewLink(withURL url: String, completionBlock: @escaping (Result<LinkModel, Error>) -> Void ) {
+        metadataDatasource.getMetadata(fromURL: url, completionBlock: completionBlock)
     }
     
     
--- SaveLink/Links/Repository/MetadataDatasource.swift
+++ SaveLink/Links/Repository/MetadataDatasource.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import LinkPresentation
+
+enum CustomMetadataError: Error {
+    case badURL
+}
+
+final class MetadataDatasource {
+    private var metadataProvider: LPMetadataProvider?
+    
+    func getMetadata(fromURL url: String, completionBlock: @escaping (Result<LinkModel, Error>) -> Void) {
+        guard let url = URL(string: url) else {
+            completionBlock(.failure(CustomMetadataError.badURL))
+            return
+        }
+        
+        metadataProvider = LPMetadataProvider()
+        metadataProvider?.startFetchingMetadata(for: url, completionHandler: { metadata, error in
+            if let error = error {
+                print("Error getting metadata \(error.localizedDescription)")
+                completionBlock(.failure(error))
+                return
+            }
+            
+            let linkModel = LinkModel(
+                url: url.absoluteString,
+                title: metadata?.title ?? "No title",
+                isFavorited: false,
+                isCompleted: false)
+            
+            completionBlock(.success(linkModel))
+        })
+    }
+}
