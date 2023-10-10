66f83f03c7fdbc72881cadc268d58e52d50cecc2
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-09 22:54:42 -0300

    Creo LinkViewModel, LinkView

---
+++ SaveLink/Links/LinkView.swift
@@ -0,0 +1,18 @@
+//
+//  LinkView.swift
+//  SaveLink
+//
+//  Created by Christian Manzaraz on 09/10/2023.
+//
+
+import SwiftUI
+
+struct LinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LinkView()
+}
