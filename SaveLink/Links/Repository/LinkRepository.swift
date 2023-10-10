b011e51c412bb4f56c68778aa27c13df7fadc66e
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-09 22:30:53 -0300

    Creo método en LinkRepository

--- SaveLink/Links/Repository/LinkRepository.swift
+++ SaveLink/Links/Repository/LinkRepository.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+final class LinkRepository {
+    private let linkDataSource: LinkDataSource
+    
+    init(linkDataSource: LinkDataSource = LinkDataSource()) {
+        self.linkDataSource = linkDataSource
+    }
+    
+    func getAllLinks(completionBlock: @escaping (Result<[LinkModel], Error>) -> Void) {
+        linkDataSource.getAllLinks(completionBlock: completionBlock)
+    }
+}
