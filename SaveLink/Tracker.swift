40a45c494c26e36b432338f02223249417f91c67
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-13 21:10:43 -0300

    Activamos FIRAnalyticsDebugEnabled

--- SaveLink/Tracker.swift
+++ SaveLink/Tracker.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import FirebaseAnalytics
+
+final class Tracker {
+    static func trackCreateLinkEvent(url: String) {
+        Analytics.logEvent("CreateLinkEvent", 
+                           parameters: ["url" : url])
+        
+    }
+    
+    static func trackerSaveLinkEvent() {
+        Analytics.logEvent("SaveLinkEvent", parameters: nil)
+    }
+    
+    static func trackErrorSaveLinkEvent(error: String) {
+        Analytics.logEvent("ErrorSaveLinkEvent", parameters: ["error": error])
+    }
+}
